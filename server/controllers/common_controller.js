0362dde970373780887c0b882207e08d9d0ca503
Lakmina Pramodya Gamage <pramodyalakmina@gmail.com>
2023-08-30 23:55:39 +0530

    initial commit

---
+++ server/controllers/common_controller.js
@@ -0,0 +1,63 @@
+const mysql = require('mysql2');
+const commonFunctions = require('./common_functions');
+const nonacademic_controller = require('./nonacademic_controller');
+
+// MYSQL CONNECTION (CHANGE .env FILE WHEN TESTING. DON'T EDIT THESE PARAMETERS)
+let conn = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_TOKEN,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+});
+
+exports.sign_in_view = async (req, res) => {
+    res.render('sign-in');
+}
+
+exports.continue_sign_in = async (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+
+
+    let loadUserDetails = (email) => {
+        return new Promise((resolve, reject) => {
+            let sql = 'SELECT id,Designation,Password FROM employees WHERE Email = "' + email + '"';
+            conn.query(sql, (err, rows) => {
+                if (!err) {
+                    return resolve(rows);
+                } else {
+                    return reject(err);
+                }
+            })
+        })
+    }
+
+    loadUserDetails(email)
+        .then((rows) => {
+            if (rows.length == 0) {
+                res.send({ status: '201', message: 'User not found' });
+            } else {
+                if (rows[0].Password == password) {
+                    process.env.CURRENT_ID = rows[0].id;
+                    process.env.CURRENT_TYPE = rows[0].Designation;
+
+                    res.send({ status: '200', type: rows[0].Designation });
+                } else {
+                    res.send({ status: '202', message: 'Incorrect password' });
+                }
+
+            }
+
+        })
+        .catch((err) => {
+            console.log(err);
+            res.send({ status: '500', message: 'Internal Server Error' })
+        });
+}
+
+exports.continue_sign_out = async (req, res) => {
+    process.env.CURRENT_ID = "";
+    process.env.CURRENT_TYPE = "";
+    res.render('sign-in');
+}
---
+++ server/routes/common_route.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const common_controller = require('../controllers/common_controller.js');
+
+// Routes for sign in
+router.get('/',common_controller.sign_in_view);
+router.get('/signin',common_controller.sign_in_view);
+router.post('/signin/contdsign',common_controller.continue_sign_in);
+router.get('/signout',common_controller.continue_sign_out);
+
+module.exports = router;
